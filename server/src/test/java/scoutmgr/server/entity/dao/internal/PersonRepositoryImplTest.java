45f0d3f4688480e27400788dd8420ab4a04c2cbf
James Walker <github@jzwalker.id.au>
2015-02-18 00:17:12 +1100

    Define test.db.driver, so that postgres is used. Need to pull this up into an abstract at some point.

--- server/src/test/java/scoutmgr/server/entity/dao/internal/PersonRepositoryImplTest.java
+++ server/src/test/java/scoutmgr/server/entity/dao/internal/PersonRepositoryImplTest.java
@@ -1,13 +1,24 @@
 package scoutmgr.server.entity.dao.internal;
 
 import java.util.List;
+import org.testng.annotations.BeforeMethod;
 import scoutmgr.server.entity.Person;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
+import org.postgresql.Driver;
 
 public class PersonRepositoryImplTest
   extends AbstractPersonRepositoryImplTest
 {
+  @BeforeMethod
+  @Override
+  public void preTest()
+    throws Exception
+  {
+    System.setProperty( "test.db.driver", Driver.class.getName() );
+    super.preTest();
+  }
+
   @Test
   public void findAllWhereNameLike()
     throws Exception
