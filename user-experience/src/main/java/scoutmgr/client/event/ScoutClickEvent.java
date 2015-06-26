b51668857c2ac5d1f72fe51fa7520c31ce9a4583
James Walker <github@jzwalker.id.au>
2015-06-26 23:00:02 +1000

    Enhance ScoutClickEvent as per suggestions on GWPT page

--- user-experience/src/main/java/scoutmgr/client/event/ScoutClickEvent.java
+++ user-experience/src/main/java/scoutmgr/client/event/ScoutClickEvent.java
@@ -2,12 +2,23 @@ package scoutmgr.client.event;
 
 import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
 import javax.annotation.Nonnull;
 import scoutmgr.client.view.model.ScoutViewModel;
 
 public class ScoutClickEvent
   extends GwtEvent<ScoutClickEvent.Handler>
 {
+  public static void fire( final com.google.gwt.event.shared.HasHandlers source, final ScoutViewModel model )
+  {
+    source.fireEvent( new ScoutClickEvent( model ) );
+  }
+
+  public interface HasHandlers
+  {
+    HandlerRegistration addScoutClickEventHandler( Handler h );
+  }
+
   public interface Handler
     extends EventHandler
   {
@@ -47,6 +58,6 @@ public class ScoutClickEvent
 
   public String toString()
   {
-    return "Scout Clicked[" + "Event=" + _scoutViewModel.getDisplayString() + "]";
+    return "Scout Clicked[" + "Scout=" + _scoutViewModel.getDisplayString() + "]";
   }
 }
