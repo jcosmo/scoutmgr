75440958b78cb09db9bf3cb8b57a9ed2d180868d
James Walker <github@jzwalker.id.au>
2016-03-28 09:49:01 +1100

    Change layout to a LayoutPanel, so that it can contain a CellTable

--- user-experience/src/main/java/scoutmgr/client/application/admin/badges/BadgesView.java
+++ user-experience/src/main/java/scoutmgr/client/application/admin/badges/BadgesView.java
@@ -1,8 +1,10 @@
 package scoutmgr.client.application.admin.badges;
 
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
@@ -21,9 +23,13 @@ public class BadgesView
   ScoutmgrResourceBundle _bundle;
 
   @UiField
-  SimplePanel _content;
+  LayoutPanel _content;
+
   @UiField
   SimplePanel _leftNav;
+
+  @UiField
+  LayoutPanel _badgesPanel;
 
   interface Binder
     extends UiBinder<Widget, BadgesView>
@@ -33,7 +39,7 @@ public class BadgesView
   @Inject
   BadgesView( final Binder uiBinder )
   {
-    initWidget( uiBinder.createAndBindUi( this ));
+    initWidget( uiBinder.createAndBindUi( this ) );
   }
 
   @Override
@@ -41,8 +47,6 @@ public class BadgesView
   {
 
   }
-
-  private static final Logger LOG = Logger.getLogger( BadgesView.class.getName() );
 
   @Override
   public void setInSlot( final Object slot, final IsWidget content )
@@ -53,7 +57,13 @@ public class BadgesView
     }
     else if ( slot == BadgesPresenter.SLOT_BADGES_CONTENT )
     {
-      _content.setWidget( content );
+      _content.clear();
+      if ( null != content )
+      {
+        _content.add( content );
+        _content.setWidgetLeftRight( content, 0, Style.Unit.PX, 0, Style.Unit.PX );
+        _content.setWidgetTopBottom( content, 0, Style.Unit.PX, 0, Style.Unit.PX );
+      }
     }
     else
     {
