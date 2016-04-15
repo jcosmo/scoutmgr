d8385e792004f225cc09e7daf88f572e6ed58c0b
James Walker <github@jzwalker.id.au>
2016-04-15 10:00:52 +1000

    Start pushing scout details to the scout display

--- user-experience/src/main/java/scoutmgr/client/application/scout/ScoutPresenter.java
+++ user-experience/src/main/java/scoutmgr/client/application/scout/ScoutPresenter.java
@@ -4,20 +4,38 @@ import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.HasUiHandlers;
 import com.gwtplatform.mvp.client.Presenter;
-import com.gwtplatform.mvp.client.PresenterWidget;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
-import java.util.logging.Logger;
-import org.realityforge.replicant.client.EntityChangeBroker;
+import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
+import org.realityforge.replicant.client.EntityRepository;
 import scoutmgr.client.application.ApplicationPresenter;
+import scoutmgr.client.entity.Person;
 import scoutmgr.client.net.ScoutmgrDataLoaderService;
 import scoutmgr.client.place.NameTokens;
+import scoutmgr.client.service.PersonnelService;
+import scoutmgr.client.view.model.ScoutViewModel;
 
 public class ScoutPresenter
   extends Presenter<ScoutPresenter.View, ScoutPresenter.Proxy>
   implements ScoutUiHandlers
 {
+  private Integer _scoutID;
+
+  @Inject
+  private PlaceManager _placeManager;
+
+
+  @Inject
+  private ScoutmgrDataLoaderService _dataloader;
+
+  @Inject
+  private EntityRepository _entityRepository;
+
+  @Inject
+  private PersonnelService _personnelService;
+
   @ProxyStandard
   @NameToken( { NameTokens.SCOUT } )
   interface Proxy
@@ -28,6 +46,9 @@ public class ScoutPresenter
   interface View
     extends com.gwtplatform.mvp.client.View, HasUiHandlers<ScoutUiHandlers>
   {
+    void setScout( ScoutViewModel viewModel );
+
+    void showLoadingMessage();
   }
 
   @Inject
@@ -38,5 +59,35 @@ public class ScoutPresenter
     super( eventBus, view, proxy, ApplicationPresenter.SLOT_MAIN_CONTENT );
 
     getView().setUiHandlers( this );
+  }
+
+  @Override
+  public void prepareFromRequest( final PlaceRequest request )
+  {
+    super.prepareFromRequest( request );
+    final String idStr = request.getParameter( "id", null );
+    if ( null != idStr )
+    {
+      _scoutID = Integer.valueOf( idStr );
+      configureForScout( _scoutID );
+    }
+    else
+    {
+      _placeManager.revealErrorPlace( "Invalid URL" );
+    }
+  }
+
+  private void configureForScout( final Integer id )
+  {
+    final Person person = _entityRepository.findByID( Person.class, id );
+    if ( null != person )
+    {
+      final ScoutViewModel viewModel = new ScoutViewModel( person );
+      getView().setScout( viewModel );
+    }
+    else
+    {
+      getView().showLoadingMessage();
+    }
   }
 }
--- user-experience/src/main/java/scoutmgr/client/application/scout/ScoutView.java
+++ user-experience/src/main/java/scoutmgr/client/application/scout/ScoutView.java
@@ -7,6 +7,7 @@ import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+import scoutmgr.client.view.model.ScoutViewModel;
 
 public class ScoutView
   extends ViewWithUiHandlers<ScoutUiHandlers>
@@ -28,5 +29,19 @@ public class ScoutView
     initWidget( uiBinder.createAndBindUi( this ) );
     _scoutDetailsPanel.add( new HTMLPanel("scout details!") );
     _otherStuff.add( new HTMLPanel("other stuff") );
+  }
+
+  @Override
+  public void setScout( final ScoutViewModel viewModel )
+  {
+    _scoutDetailsPanel.clear();
+    _scoutDetailsPanel.add( new HTMLPanel( "Scout: " + viewModel.getFirstName() + " " + viewModel.getLastName() ) );
+  }
+
+  @Override
+  public void showLoadingMessage()
+  {
+    _scoutDetailsPanel.clear();
+    _scoutDetailsPanel.add( new HTMLPanel("Loading....") );
   }
 }
