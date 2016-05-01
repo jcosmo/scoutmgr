19426227c09bda5bad21ce587c161092129a010e
James Walker <github@jzwalker.id.au>
2016-05-02 00:09:43 +1000

    Display cards for each badge in a category

--- user-experience/src/main/java/scoutmgr/client/application/scout/badgework/BadgeworkView.java
+++ user-experience/src/main/java/scoutmgr/client/application/scout/badgework/BadgeworkView.java
@@ -6,13 +6,22 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+import gwt.material.design.client.constants.IconPosition;
+import gwt.material.design.client.constants.IconType;
+import gwt.material.design.client.ui.MaterialCard;
+import gwt.material.design.client.ui.MaterialCardAction;
+import gwt.material.design.client.ui.MaterialCardContent;
+import gwt.material.design.client.ui.MaterialCardTitle;
 import gwt.material.design.client.ui.MaterialCollapsible;
 import gwt.material.design.client.ui.MaterialCollapsibleBody;
 import gwt.material.design.client.ui.MaterialCollapsibleHeader;
 import gwt.material.design.client.ui.MaterialCollapsibleItem;
+import gwt.material.design.client.ui.MaterialColumn;
 import gwt.material.design.client.ui.MaterialLink;
+import gwt.material.design.client.ui.MaterialRow;
 import java.util.ArrayList;
 import java.util.Collections;
+import scoutmgr.client.entity.Badge;
 import scoutmgr.client.entity.BadgeCategory;
 
 public class BadgeworkView
@@ -39,22 +48,48 @@ public class BadgeworkView
     Collections.sort( badgeCategories, ( o1, o2 ) -> o1.getRank() - o2.getRank() );
     for ( final BadgeCategory badgeCategory : badgeCategories )
     {
-      _expandable.add( createItem( badgeCategory ) );
+      _expandable.add( createBadgeCategoryView( badgeCategory ) );
     }
   }
 
-  private MaterialCollapsibleItem createItem( final BadgeCategory badgeCategory)
+  private MaterialCollapsibleItem createBadgeCategoryView( final BadgeCategory badgeCategory )
   {
-    MaterialCollapsibleItem item = new MaterialCollapsibleItem();
-    MaterialCollapsibleHeader header = new MaterialCollapsibleHeader();
-    MaterialLink link = new MaterialLink();
+    final MaterialCollapsibleItem item = new MaterialCollapsibleItem();
+    final MaterialCollapsibleHeader header = new MaterialCollapsibleHeader();
+    final MaterialLink link = new MaterialLink();
     link.setText( badgeCategory.getName() );
     header.add( link );
     item.add( header );
-    MaterialCollapsibleBody body = new MaterialCollapsibleBody(  );
-    body.add(new HTML("this is some content!"));
-    item.add(body);
+    final MaterialCollapsibleBody body = new MaterialCollapsibleBody();
+    body.add( createBadgesView( badgeCategory ) );
+    item.add( body );
     return item;
+  }
+
+  private Widget createBadgesView( final BadgeCategory badgeCategory )
+  {
+    final MaterialRow row = new MaterialRow();
+    for ( final Badge badge : badgeCategory.getBadges() )
+    {
+      final MaterialColumn column = new MaterialColumn();
+      column.setGrid( "s12 m4 l3" );
+      final MaterialCard card = new MaterialCard();
+      final MaterialCardContent cardContent = new MaterialCardContent();
+      final MaterialCardTitle cardTitle = new MaterialCardTitle();
+      cardTitle.setText( badge.getName() );
+      cardTitle.setIconType( IconType.POLYMER );
+      cardTitle.setIconPosition( IconPosition.RIGHT );
+      cardContent.add( cardTitle );
+      cardContent.add( new HTML("Blurb about this badge!"));
+      final MaterialCardAction actions = new MaterialCardAction();
+      final MaterialLink progressLink = new MaterialLink( "Record Progress" );
+      actions.add( progressLink );
+      cardContent.add( actions );
+      card.add( cardContent );
+      column.add(card);
+      row.add( column );
+    }
+    return row;
   }
 
   @Override
