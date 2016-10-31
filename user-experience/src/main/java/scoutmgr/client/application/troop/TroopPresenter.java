5828f7a373741e808faf37706595308513f09f3f
James Walker <github@jzwalker.id.au>
2016-11-01 10:36:30 +1100

    WIP - adding troup view

--- user-experience/src/main/java/scoutmgr/client/application/troop/TroopPresenter.java
+++ user-experience/src/main/java/scoutmgr/client/application/troop/TroopPresenter.java
@@ -6,14 +6,25 @@ import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import javax.inject.Inject;
+import org.realityforge.replicant.client.EntityRepository;
 import scoutmgr.client.application.ApplicationPresenter;
+import scoutmgr.client.entity.Person;
 import scoutmgr.client.place.NameTokens;
+import scoutmgr.client.view.model.ScoutViewModel;
 
 public class TroopPresenter
   extends Presenter<TroopPresenter.View, TroopPresenter.Proxy>
   implements TroopUiHandlers
 {
+  private final HashMap<Person, ScoutViewModel> _model2ViewModel = new HashMap<>();
+
+  @Inject
+  private EntityRepository _entityRepository;
+
   @ProxyStandard
   @NameToken( { NameTokens.TROOP } )
   interface Proxy
@@ -25,6 +36,7 @@ public class TroopPresenter
   interface View
     extends com.gwtplatform.mvp.client.View, HasUiHandlers<TroopUiHandlers>
   {
+    void setScouts( Collection<ScoutViewModel> values );
   }
 
   @Inject
@@ -35,5 +47,25 @@ public class TroopPresenter
     super( eventBus, view, proxy, ApplicationPresenter.SLOT_MAIN_CONTENT );
 
     getView().setUiHandlers( this );
+  }
+
+  @Override
+  protected void onReveal()
+  {
+    _model2ViewModel.clear();
+    getView().setScouts( _model2ViewModel.values() );
+    super.onReveal();
+  }
+
+  private void initialiseViewModel()
+  {
+    _model2ViewModel.clear();
+    final ArrayList<Person> scouts = _entityRepository.findAll( Person.class );
+    for ( final Person scout : scouts )
+    {
+      _model2ViewModel.put( scout, new ScoutViewModel( scout ) );
+    }
+
+    getView().setScouts( _model2ViewModel.values() );
   }
 }
--- user-experience/src/main/java/scoutmgr/client/application/troop/TroopView.java
+++ user-experience/src/main/java/scoutmgr/client/application/troop/TroopView.java
@@ -1,17 +1,59 @@
 package scoutmgr.client.application.troop;
 
+import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.ColumnSortEvent;
+import com.google.gwt.user.cellview.client.DataGrid;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy;
+import com.google.gwt.user.cellview.client.SimplePager;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.view.client.NoSelectionModel;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
-import gwt.material.design.client.ui.MaterialTab;
+import java.util.Collection;
+import java.util.Comparator;
 import javax.inject.Inject;
+import scoutmgr.client.resource.ScoutmgrResourceBundle;
+import scoutmgr.client.view.model.ScoutViewModel;
 
 public class TroopView
   extends ViewWithUiHandlers<TroopUiHandlers>
   implements TroopPresenter.View
 {
+  private final ListDataProvider<ScoutViewModel> _provider;
+  @UiField
+  LayoutPanel _scoutsPanel;
+  @UiField( provided = true )
+  DataGrid<ScoutViewModel> _scoutsTable;
+  @UiField
+  SimplePanel _pagerPanel;
+  @UiField
+  ScoutmgrResourceBundle _bundle;
+
+  /*
+    interface DataGridStyle
+      extends DataGrid.Style
+    {
+      String left();
+
+      String headerHighlight();
+    }
+
+    interface DataGridResources
+      extends DataGrid.Resources
+    {
+      @Override
+      @Source( "TroopDataGrid.gss" )
+      TroopView.DataGridStyle dataGridStyle();
+    }
+  */
   interface Binder
     extends UiBinder<Widget, TroopView>
   {
@@ -20,6 +62,68 @@ public class TroopView
   @Inject
   TroopView( final Binder uiBinder )
   {
+    _scoutsTable = new DataGrid<>( 20 );
+    _scoutsTable.setAutoHeaderRefreshDisabled( false );
+    _scoutsTable.setEmptyTableWidget( new Label( "No scouts" ) );
+
+    _provider = new ListDataProvider<>();
+
+    final Column<ScoutViewModel, String> nameColumn =
+      new Column<ScoutViewModel, String>( new TextCell() )
+      {
+        @Override
+        public String getValue( final ScoutViewModel viewModel )
+        {
+          return getScoutName( viewModel );
+        }
+      };
+    createColumnSorters( nameColumn );
+
+    _scoutsTable.addColumn( nameColumn, SafeHtmlUtils.fromString( "Username" ) );
+    final NoSelectionModel<ScoutViewModel> selectionModel = new NoSelectionModel<>();
+    _scoutsTable.setSelectionModel( selectionModel );
+    _scoutsTable.setKeyboardSelectionPolicy( HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.DISABLED );
+
+
+    _provider.addDataDisplay( _scoutsTable );
+
     initWidget( uiBinder.createAndBindUi( this ) );
+
+    setupPager();
+  }
+
+  private String getScoutName( final ScoutViewModel viewModel )
+  {
+    return viewModel.getLastName() + ", " + viewModel.getFirstName();
+  }
+
+  private void setupPager()
+  {
+    final SimplePager.Resources pagerResources = GWT.create( SimplePager.Resources.class );
+    final SimplePager pager = new SimplePager( SimplePager.TextLocation.CENTER, pagerResources, false, 0, true );
+    pager.setDisplay( _scoutsTable );
+    _pagerPanel.setWidget( pager );
+  }
+
+  private void createColumnSorters( final Column<ScoutViewModel, String> nameColumn )
+  {
+    final ColumnSortEvent.ListHandler<ScoutViewModel> sortHandler =
+      new ColumnSortEvent.ListHandler<>( _provider.getList() );
+    _scoutsTable.addColumnSortHandler( sortHandler );
+
+    nameColumn.setSortable( true );
+    sortHandler.setComparator( nameColumn,
+                               ( o1, o2 ) ->
+                                 getScoutName( o1  ).compareTo( getScoutName( o2 )) );
+  }
+
+  public void setScouts( final Collection<ScoutViewModel> list )
+  {
+    _provider.getList().clear();
+    if ( null != list )
+    {
+      _provider.getList().addAll( list );
+    }
+    ColumnSortEvent.fire( _scoutsTable, _scoutsTable.getColumnSortList() );
   }
 }
